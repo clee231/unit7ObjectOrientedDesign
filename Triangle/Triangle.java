068ac76b934e9352c5ee06c01b0411897cf0f8e2
LeeBry <Echoslee@yahoo.com>
2016-02-16 14:14:47 -0600

    Ugh
    painting component

--- Triangle/Triangle.java
+++ Triangle/Triangle.java
@@ -4,8 +4,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 
+// need a class that will keep track of the points and draw them in. (ie Cityscape)
 /**
  * Draws a triangle using the GUI to pick the 3 points on the given screen size
  * @author (Bryce Lee) 
@@ -13,14 +13,12 @@ import java.util.ArrayList;
  */
 public class Triangle extends JFrame
 {
-    private static final int FRAME_WIDTH= 350;// in pixels
-    private static final int FRAME_HEIGHT= 200;
-    
-    private int point1;
-    private int point2;
-    private int point3;
-    
+    private static final int FRAME_WIDTH= 500;
+    private static final int FRAME_HEIGHT= 500;
+    // For the Window Size
+   
     private JFrame frame;
+    // For  the components of the frame
     /**
      * Constructor for objects of class Triangle
      */
@@ -28,7 +26,10 @@ public class Triangle extends JFrame
     {      
         this.frame= new JFrame();
         MousePressListener listener= new MousePressListener();
+        this.frame.addMouseListener(listener);
         
+        TriangleDraw draw= new TriangleDraw();
+        this.frame.add(draw);
         
         this.frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
@@ -43,15 +44,15 @@ public class Triangle extends JFrame
   
     public class MousePressListener implements MouseListener
     {
-        public void mousePressed(MouseEvent event)
+        public void mousePressed(MouseEvent event){ }
+        public void mouseReleased(MouseEvent event){}
+        public void mouseClicked(MouseEvent event)
         {
             int x= event.getX();
             int y= event.getY();
         }
-        void mouseReleased(MouseEvent event);
-        void mouseClicked(MouseEvent event);
-        void mouseEntered(MouseEvent event);
-        void mouseExited(MouseEvent event);
+        public void mouseEntered(MouseEvent event){}
+        public void mouseExited(MouseEvent event){}
     }
 
 }
---
+++ Triangle/TriangleDraw.java
@@ -0,0 +1,44 @@
+import javax.swing.JFrame;
+import javax.swing.JComponent;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Ellipse2D;
+
+/**
+ * Draws the triangle with the 3 given points
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TriangleDraw extends JComponent 
+{
+   
+    /**
+     *@param    g2 the graphics context
+     */
+    public void paintComponent(Graphics g)
+    {
+        Graphics2D g2 = (Graphics2D) g;
+
+        
+    }
+    public void point (Graphics2D g2)
+    {
+      //this will draw a point on the screen
+      Ellipse2D ellipse = new Ellipse2D.Double();
+      g2.draw(new Ellipse2D.Double(1,1,2,2)); // need to be specific
+    }
+    public void line(Graphics2D g2)
+    {
+        //will draw the lines when clicked
+        Line2D line= new Line2D.Double();
+    }
+    public void nextFrame()
+    {
+        // request that the Java Runtime repaints this component by invoking its paintComponent method
+        repaint();
+    }
+}
